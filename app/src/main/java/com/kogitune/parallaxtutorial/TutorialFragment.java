cd2fe029c3fd42aa1f60076448ed5c390b260e16
takahirom <takam.dev@gmail.com>
2015-07-02 00:49:52 +0900

    add color to page fragment

--- app/src/main/java/com/kogitune/parallaxtutorial/TutorialFragment.java
+++ app/src/main/java/com/kogitune/parallaxtutorial/TutorialFragment.java
@@ -1,5 +1,6 @@
 package com.kogitune.parallaxtutorial;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -22,7 +23,17 @@ public class TutorialFragment extends Fragment {
         viewPager.setAdapter(new FragmentPagerAdapter(getFragmentManager()) {
             @Override
             public Fragment getItem(int i) {
-                return new TutorialPageFragment();
+                final TutorialPageFragment tutorialPageFragment = new TutorialPageFragment();
+                final Bundle bundle = new Bundle();
+                if (i == 0) {
+                    bundle.putInt("color", Color.RED);
+                } else if (i == 1) {
+                    bundle.putInt("color", Color.GREEN);
+                } else {
+                    bundle.putInt("color", Color.YELLOW);
+                }
+                tutorialPageFragment.setArguments(bundle);
+                return tutorialPageFragment;
             }
 
             @Override
@@ -52,7 +63,12 @@ public class TutorialFragment extends Fragment {
             if (savedInstanceState != null) {
                 return null;
             }
-            return inflater.inflate(R.layout.tutorial_page, container, false);
+            final View view = inflater.inflate(R.layout.tutorial_page, container, false);
+            final Bundle arguments = getArguments();
+            if (arguments != null) {
+                view.setBackgroundColor(arguments.getInt("color"));
+            }
+            return view;
         }
     }
 }
